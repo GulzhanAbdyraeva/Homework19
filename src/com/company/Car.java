c4ff33bdec561705a8e7f0b05622a33e6fcab46c
user <imaginejuli@gmail.com>
2022-06-01 18:14:35 +0300

    Initial commit

---
+++ src/com/company/Car.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Car implements AutoCloseable {
+
+    public void drive(){
+        System.out.println("Mashina jurup jatat.");
+    }
+    @Override
+    public void close() throws Exception {
+        System.out.println("Mashina jabylyp jatat.");
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.io.UncheckedIOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try (Car car = new Car()) {
+            car.drive();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
